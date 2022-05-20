3aef21831da811a78db9b3652bb607a3738457b0
JoacoML <joaco.martin.lanfranchi@gmail.com>
2022-05-20 17:45:01 +0200

    Estructura de Log in

--- js/script.js
+++ js/script.js
@@ -1,56 +1,56 @@
 //////// Log in ////////
 
-// // Nombre del Usuario
-// let usuario = prompt("Escribe tu usuario");
-// // Saludo
-// let fecha = new Date(); 
-// let hora = fecha.getHours();
-// let saludo = ""
-// let intentos = 5
+// Nombre del Usuario
+let usuario = prompt("Escribe tu usuario");
+// Saludo
+let fecha = new Date(); 
+let hora = fecha.getHours();
+let saludo = ""
+let intentos = 5
   
-// function saludar(){
-//   alert(saludo + " " + usuario);
-// }
+function saludar(){
+  alert(saludo + " " + usuario);
+}
 
-// if(hora >= 0 && hora < 12){
-//   saludo = "Buenos días";
-// }else if(hora >= 12 && hora < 19){
-//   saludo = "Buenas tardes";
-// }else if(hora >= 19 && hora < 24){
-//   saludo = "Buenas noches";
-// }
+if(hora >= 0 && hora < 12){
+  saludo = "Buenos días";
+}else if(hora >= 12 && hora < 19){
+  saludo = "Buenas tardes";
+}else if(hora >= 19 && hora < 24){
+  saludo = "Buenas noches";
+}
 
-// saludar();
+saludar();
 
-// // Ingresar pass
-// let pass;
-// let x = 0;
-// do {
-//   if (x === 5) {
-//     alert("Su cuenta ha sido bloqueada");
-//     break;
-//   } else {
-//     function resta (intentos, x){
-//       let intentosRestantes;
-//       intentosRestantes = intentos - x;
-//       return intentosRestantes
-//     } 
-//     let intentosRestantes = resta (intentos, x);
+// Ingresar pass
+let pass;
+let x = 0;
+do {
+  if (x === 5) {
+    alert("Su cuenta ha sido bloqueada");
+    break;
+  } else {
+    function resta (intentos, x){
+      let intentosRestantes;
+      intentosRestantes = intentos - x;
+      return intentosRestantes
+    } 
+    let intentosRestantes = resta (intentos, x);
 
-//     if (x > 0) {
-//       alert("Intentos restantes: " + intentosRestantes +"." + " Luego se bloqueara la cuenta!")
-//     }
-//     x++;
-//   }
-//   pass = prompt("escribe tu contraseña");
+    if (x > 0) {
+      alert("Intentos restantes: " + intentosRestantes +"." + " Luego se bloqueara la cuenta!")
+    }
+    x++;
+  }
+  pass = prompt("escribe tu contraseña");
 
-// } while (pass !== "12345");
+} while (pass !== "12345");
 
-// if (pass === "12345") {
-//   alert("Hola " + usuario + ", " + "bienvenido a ACTIONS");
-// } else {
-//   alert("Chau " + usuario + ", vuelva pronto!");
-// }
+if (pass === "12345") {
+  alert("Hola " + usuario + ", " + "bienvenido a ACTIONS");
+} else {
+  alert("Chau " + usuario + ", vuelva pronto!");
+}
 
 //////// Proyectos ////////
 
@@ -131,7 +131,7 @@ function projectsMenu() {
 
         carrito[index].addToPorfolio();
         carrito[index].actualizarPrecioTotal();
-        
+
         alert(`
         Otra accion de ${carrito[index].title} 🎉
         Unidades: ${carrito[index].cantidad}`);
