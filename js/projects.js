06397804f4abfe66d1365adffb84b4486e160b87
JoacoML <joaco.martin.lanfranchi@gmail.com>
2022-07-03 02:20:52 +0200

    redesign de htmls y funcion addtocart

--- js/projects.js
+++ js/projects.js
@@ -26,20 +26,20 @@ const conteinerProject = document.getElementById('conteinerProject')
 const conteinerCarrito = document.getElementById('conteinerCarrito')
 const vaciarCarrito = document.getElementById('vaciarCarrito')
 const pagarCarrito = document.getElementById('pagarCarrito')
-const totalPriceCarrito = document.getElementById('totalPriceCarrito')
+
+
 let carrito = [];
 
-// Get local storage
-// Como puedo simplificar este codigo con un OR?
-// const carrito = JSON.parse(localStorage.getItem('carrito')) || []
+const totalCarrito = document.getElementById('totalCarrito').innerHTML = carrito.length;
 
+// Get local storage
 document.addEventListener('DOMContentLoaded', () => {
   if (localStorage.getItem('carrito')){
-    carrito = JSON.parse(localStorage.getItem('carrito'))
+    carrito = JSON.parse(localStorage.getItem('carrito'));
     printCart()
+    console.log(carrito)
   }
 })
-
 // Impresion del Stock
 
 function printStock(){
@@ -48,7 +48,6 @@ function printStock(){
     .then(resultado => resultado.json())
     .then(data =>{
       projectStock = data;
-      console.log(projectStock)
       projectStock.forEach((project) => {
         let card = document.createElement('div')
         card.innerHTML = `
@@ -76,15 +75,52 @@ function printStock(){
 
 printStock();
 
+// Funcion eliminar del carrito
+const deleteCart = (projectId) => {
+  const item = carrito.find((project) => project.id === projectId);
+  const index = carrito.indexOf(item); 
+  carrito.splice(index,1);
+  printCart()
+}
+
+// Funcion borrar carrito
+vaciarCarrito.addEventListener('click', () => {
+  carrito.length = 0
+  printCart();
+})
+
+// Funcion pagar carrito
+pagarCarrito.addEventListener('click', () => {
+  alert(`Tu inversion total es de U$$${totalBuy}. Gracias! 😄`);
+  carrito.length = 0;
+  printCart();
+})
+
 // Funcion argegar al carrito
 
 const addToCart = (projectId) => {
+
   const inCart = carrito.find((project) => project.id === projectId);
 
   if (inCart) {
-    let index = carrito.findIndex((project) => project.id === inCart.id);
-    carrito[index].addToPorfolio();
-    carrito[index].actualizarPrecioTotal();
+
+    function addToPorfolio(){
+      inCart.cantidad++;
+    }
+    addToPorfolio();
+
+    function actualizarPrecioTotal(){
+      inCart.totalPrice = inCart.price * inCart.cantidad;
+    }
+    
+    actualizarPrecioTotal()
+    
+    // const index = carrito.findIndex((project) => project.id === inCart.id);
+    // carrito[index].addToPorfolio();
+    // carrito[index].actualizarPrecioTotal();
+
+    // inCart.addToPorfolio();
+    // inCart.actualizarPrecioTotal();
 
     Toastify({
       text:`Otra accion de ${inCart.title} 🎉`,
@@ -95,51 +131,28 @@ const addToCart = (projectId) => {
     }).showToast();
 
   } else {
-  let newProject = projectStock.find((project) => project.id === projectId);
-  carrito.push(new Project(newProject));
-  carrito[carrito.length - 1].actualizarPrecioTotal();
-  
-  
-  Toastify({
-    text:`Una accion de ${newProject.title} enviada al carrito 🎉`,
-    duration:3000,
-    gravity:'top',
-    className: "notiCarrito",
-    position:'center'
-  }).showToast();
+    let newProject = projectStock.find((project) => project.id === projectId);
+    carrito.push(new Project(newProject));
+    carrito[carrito.length - 1].actualizarPrecioTotal();
 
-}
+    Toastify({
+      text:`Una accion de ${newProject.title} enviada al carrito 🎉`,
+      duration:3000,
+      gravity:'top',
+      className: "notiCarrito",
+      position:'center'
+    }).showToast();
+  }
 printCart()
 }
 
-// Funcion eliminar del carrito
-const deleteCart = (projectId) => {
-  const item = carrito.find((project) => project.id === projectId);
-  const index = carrito.indexOf(item); 
-  carrito.splice(index,1);
-  printCart()
-} 
-
-// Funcion borrar carrito
-vaciarCarrito.addEventListener('click', () => {
-  carrito.length = 0
-  printCart()
-})
-
-// Funcion pagar carrito
-pagarCarrito.addEventListener('click', () => {
-  alert(`Tu inversion total es de U$$${totalCarrito}. Gracias! 😄`);
-  carrito.length = 0;
-  printCart();
-})
-
 // Impresion del carrito
 
-const printCart = () => {
-  conteinerCarrito.innerHTML = ""
+function printCart() {
+  conteinerCarrito.innerHTML = "";
 
   carrito.forEach((project) => {
-    let card = document.createElement('div')
+    let card = document.createElement('div');
     card.innerHTML = `
     <figure class="card m-3">
       <h3 class="card__title">${project.title}</h3>
@@ -153,22 +166,25 @@ const printCart = () => {
       <div class="btn-group buttonCard-eliminar" role="group" aria-label="Invesments kart">
         <button id="delete${project.id}" type="button" class="btn btn-danger"> Delete </button>
       </div>
-    </figure>`
+    </figure>`;
     conteinerCarrito.appendChild(card);
 
-    const botonDelete = document.getElementById(`delete${project.id}`)
-  
-    botonDelete.addEventListener('click', () => {
-      deleteCart(project.id)
-    })
+    const botonDelete = document.getElementById(`delete${project.id}`);
 
-  })
-  
+    botonDelete.addEventListener('click', () => {
+      deleteCart(project.id);
+    });
+
+  });
+
+  // total price carrito
+  totalBuy = document.getElementById('totalBuy').innerHTML = carrito.reduce((total, elemento) => total + elemento.totalPrice, 0);
+
   // Set local storage
   localStorage.setItem('carrito', JSON.stringify(carrito))
 
-  totalPriceCarrito.innerText = carrito.reduce((total, elemento) => total + elemento.totalPrice, 0);
-  totalCarrito = carrito.reduce((total, elemento) => total + elemento.totalPrice, 0);
+  // totalCart 
+  let totalCarrito = document.getElementById('totalCarrito').innerHTML = carrito.length;
+
+
 }
-
-
